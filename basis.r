07dd681cce4cf07b33c322e73bb76663450051bd
Andrey Tatarinov <elephantropy@gmail.com>
2013-09-15 16:39:57 +0400

    some plots

--- basis.r
+++ basis.r
@@ -1,12 +1,12 @@
 library('rjson')
 library('RCurl')
 
-getBasisData <- function() {
+getBasisData <- function(date='2013-09-14') {
   user_id <- readLines('user_id.txt')
   
   basis.url <- sprintf('https://app.mybasis.com/api/v1/chart/%s.json?summary=true&interval=60&units=ms&start_date=%s&start_offset=-10800&end_offset=10800&heartrate=true&steps=true&calories=true&gsr=true&skin_temp=true&air_temp=true&bodystates=true',
                  user_id, 
-                 '2013-09-14')
+                 date)
   
   basis.data.string <- getURL(basis.url)
   
@@ -31,3 +31,23 @@ getBasisData <- function() {
 
   return(basis.data)
 }
+
+basis.data.13 <- getBasisData('2013-09-13')
+basis.data.14 <- getBasisData('2013-09-14')
+
+ts <- basis.data.13$timestamp - basis.data.13$timestamp[1]
+
+hr.ts.13 <- basis.data.13$timestamp[!is.na(basis.data.13$heartrate)] - basis.data.13$timestamp[1]
+hr.13 <- basis.data.13$heartrate[!is.na(basis.data.13$heartrate)]
+
+spl.13 <- smooth.spline(x=hr.ts.13, y=hr.13)
+
+hr.ts.14 <- basis.data.14$timestamp[!is.na(basis.data.14$heartrate)] - basis.data.14$timestamp[1]
+hr.14 <- basis.data.14$heartrate[!is.na(basis.data.14$heartrate)]
+
+spl.14 <- smooth.spline(x=hr.ts.14, y=hr.14)
+
+plot(x=hr.ts.13, y=hr.13, col='grey')
+lines(x=hr.ts.14, y=hr.14, col='grey', type='p')
+lines(spl.13, col='red')
+lines(spl.14, col='blue')
